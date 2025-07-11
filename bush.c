2c742e2f099c55936d813e6b78311fb25cd7b254
piradiusquared <kpan5121@gmail.com>
2025-07-11 14:23:49 +1000

    bush.c changes:
    General changes:
    - Added required include statements
    - Include header file bush.h
    - MAX_BUFF used as temporary buffer limit
    
    Function changes:
        getInput():
        - Infinite loop to get user input from stdin
        - getline() used to get input and process accordingly (i.e. replacing new line for EOF at end of input)
        - Handles two basic commands: pwd and clear. Afterwards, prints out the input to stdout to the user
        - Handles ^D gracefully, and clears all memory used by getline()
    
        printDir():
        - Basic function to get current directory using getcwd()
        - Prints directory to stdout
    
        main():
        - Clears terminal with clear() found in bush.h
        - Greets world with hello world
        - Calls getInput() to infinitely take user input

--- bush.c
+++ bush.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "bush.h"
+
+#define MAX_BUFF 1024
+
+/**
+ * Infinite loop to get user input
+ * Handle input by calling helper functions to parse and exec commands where
+ * applicable
+*/
+void getInput(void)
+{
+    char *line;
+    size_t buffLen = 0;
+    ssize_t lineLen;
+
+    printf(">>> ");
+    while ((lineLen = getline(&line, &buffLen, stdin)) != EOF) {
+        if (lineLen > 0) {
+            line[lineLen - 1] = '\0';
+        }
+        if (!line[0] || !line) {
+            continue;
+        }
+        // Execute the program here
+        if (strcmp(line, "pwd") == 0) {
+            printDir();
+        } else if (strcmp(line, "clear") == 0) {
+            clear();
+        }
+        printf("Your input was %s\n", line);
+        printf(">>> ");
+    }
+    printf("Exiting Shell...\n");
+    free(line);
+}
+
+/**
+ * Function to print current directory
+*/
+void printDir(void)
+{
+    char cwd[MAX_BUFF];
+
+    getcwd(cwd, sizeof(cwd));
+    printf("%s\n", cwd);
+}
 
 int main(int argc, char* argv[])
 {
+    clear(); // Clear terminal
     printf("Hello world!\n");
+    getInput(); // Infinite loop
 
     return 0;
 }
